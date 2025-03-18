b62f324d46da206e94cba32c10bab1a414cf0d92
王文涛 <@LAPTOP-6A69VN6P>
2025-03-18 10:53:40 +0800

    完成任务14：使用`AtomicInteger`类进行线程安全的整数操作，避免同步问题。

---
+++ src/main/java/com/concurrent/exercise/exercise04/Main.java
@@ -0,0 +1,16 @@
+package com.concurrent.exercise.exercise04;
+
+/**
+ * @author 王文涛
+ * @date 2025/3/17
+ * @description
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
